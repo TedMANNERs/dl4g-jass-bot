c9ed0b2adf0ddaf81325fd49d98cb7cb5135c9b8
TedMANNERs <mannersofted@gmail.com>
2020-11-01 11:57:58 +0100

    implement random hand sampling

--- mcts/mcts.py
+++ mcts/mcts.py
@@ -36,26 +36,42 @@ class MonteCarloTreeSearch:
             cards_in_play[c] = 0
         other_player_cards = cards_in_play - self.root.obs.current_trick - self.root.obs.hand
 
-        MAX_CARDS_COUNT = 9
-        my_card_count = len(np.flatnonzero(self.root.obs.hand))
-
         hands = np.zeros(shape=[4, 36])
         for player in range(MAX_PLAYER + 1):
             if player == self.root.obs.player:
                 hands[player] = self.root.obs.hand
             else:
                 hands[player], other_player_cards = self._get_random_hand(other_player_cards, player)
+        print(other_player_cards)
         return hands
 
     def _get_random_hand(self, other_player_cards, player):
-         1 2 3 4
-           x
-         c
-        nr_of_next_players_with_more_cards = 3 - len(self.root.obs.current_trick)
-        # TODO: calculate
+        # use ugly hack because current_trick contains -1 for unplayed cards
+        current_trick_cards = np.flatnonzero(self.root.obs.current_trick + 1) - 1
+        nr_of_previous_players_with_less_cards = len(current_trick_cards)
+
+        previous_player = [1, 2, 3, 0]
+
+        # all players ahead of us receive the same number of cards
+        nr_of_cards_to_receive = len(np.flatnonzero(self.root.obs.hand))
+
+        # all previous players receive one less
+        previous_p = self.root.obs.player
+        for x in range(nr_of_previous_players_with_less_cards):
+            previous_p = previous_player[previous_p]
+            if previous_p == player:
+                # 1 less card because they already played a card
+                nr_of_cards_to_receive -= 1
+
         # Choose random cards from other_player_cards
-        nr_of_cards = len(np.flatnonzero(other_player_cards)) / 3
-        return [], other_player_cards
+        hand_to_receive = np.zeros(36)
+        cards_to_receive = np.random.choice(np.flatnonzero(other_player_cards), nr_of_cards_to_receive)
+        for card in cards_to_receive:
+            hand_to_receive[card] = 1
+            # remove the cards from other_player_cards so the next player cannot receive the same cards
+            other_player_cards[card] = 0
+
+        return hand_to_receive, other_player_cards
 
     def _select_next_node(self):
         node = self.root
